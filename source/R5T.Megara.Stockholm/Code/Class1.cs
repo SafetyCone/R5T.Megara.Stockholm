a8f7eaeec24501975fa2ee1f99bc6e610a60ca83
MinexAutomation <minex.automation@gmail.com>
2020-01-23 12:05:50 -0800

    Added StreamFileSerializer<T> implementation of the IFileSerialize<T> service.

--- source/R5T.Megara.Stockholm/Code/Class1.cs
+++
@@ -1,8 +0,0 @@
-﻿using System;
-
-namespace R5T.Megara.Stockholm
-{
-    public class Class1
-    {
-    }
-}
---
+++ source/R5T.Megara.Stockholm/Code/Services/Implementations/StreamFileSerializer.cs
@@ -0,0 +1,36 @@
+﻿using System;
+
+using R5T.Magyar.IO;
+using R5T.Stockholm;
+
+
+namespace R5T.Megara.Stockholm
+{
+    public class StreamFileSerializer<T> : IFileSerializer<T>
+    {
+        private IStreamSerializer<T> StreamSerializer { get; }
+
+
+        public StreamFileSerializer(IStreamSerializer<T> streamSerializer)
+        {
+            this.StreamSerializer = streamSerializer;
+        }
+
+        public T Deserialize(string filePath)
+        {
+            using (var fileStream = FileStreamHelper.NewRead(filePath))
+            {
+                var value = this.StreamSerializer.Deserialize(fileStream);
+                return value;
+            }
+        }
+
+        public void Serialize(string filePath, T value, bool overwrite = true)
+        {
+            using (var fileStream = FileStreamHelper.NewWrite(filePath, overwrite))
+            {
+                this.StreamSerializer.Serialize(fileStream, value);
+            }
+        }
+    }
+}
